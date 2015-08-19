922a1d7ec66ef6ac74175cf8e51fc298200f025c
Jack Pappas <jack-pappas@users.noreply.github.com>
2015-08-18 20:44:13 -0400

    Optimize checkPathForIllegalChars so it doesn't allocate a closure each time it's called. Modified functions in the Internal.Utilities.Filename module so they only call checkPathForIllegalChars when it's highly likely the path contained an illegal character.

--- src/utils/filename.fs
+++ src/utils/filename.fs
@@ -7,40 +7,94 @@ open Microsoft.FSharp.Compiler.AbstractIL.Internal.Library
 
 exception IllegalFileNameChar of string * char
 
-let illegalPathChars = Path.GetInvalidPathChars()
+/// The set of characters which may not be used in a path.
+let illegalPathChars =
+    let chars = Path.GetInvalidPathChars ()
+    Array.sortInPlace chars
+    chars
 
-let checkPathForIllegalChars (path:string) = 
-    for c in path do
-        if illegalPathChars |> Array.exists(fun c1->c1=c) then 
-            raise(IllegalFileNameChar(path,c))
+let checkPathForIllegalChars (path:string) =
+    let len = path.Length
+    for i = 0 to len - 1 do
+        // The current character in the string.
+        let c = path.[i]
+
+        // Determine if this character is disallowed within a path by
+        // attempting to find it in the array of illegal path characters.
+        for badChar in illegalPathChars do
+            if c = badChar then
+                // The character is not allowed to be used within a path, raise an exception.
+                raise(IllegalFileNameChar(path, c))
 
 // Case sensitive (original behaviour preserved).
 let checkSuffix (x:string) (y:string) = x.EndsWith(y,System.StringComparison.Ordinal) 
 
 let hasExtension (s:string) = 
-    checkPathForIllegalChars s
-    (s.Length >= 1 && s.[s.Length - 1] = '.' && s <> ".." && s <> ".") 
-    || Path.HasExtension(s)
+    let sLen = s.Length
+    if sLen >= 1 && s.[sLen - 1] = '.' && s <> ".." && s <> "." then true
+    else
+        try Path.HasExtension(s)
+        with
+        | :? System.ArgumentException ->
+            // Suppress the original exception and let 'checkPathForIllegalChars' raise a new one.
+            checkPathForIllegalChars s
+            // In case the exception was raised for some other reason.
+            reraise ()
 
 let chopExtension (s:string) =
-    checkPathForIllegalChars s
     if s = "." then "" else // for OCaml compatibility
     if not (hasExtension s) then 
         raise (System.ArgumentException("chopExtension")) // message has to be precisely this, for OCaml compatibility, and no argument name can be set
-    Path.Combine (Path.GetDirectoryName s,Path.GetFileNameWithoutExtension(s))
+
+    try Path.Combine (Path.GetDirectoryName s,Path.GetFileNameWithoutExtension(s))
+    with
+    | :? System.ArgumentException ->
+        // Path.GetDirectoryName(string) can raise ArgumentException when the string is empty or whitespace.
+        // Check for that condition and propagate the original exception if it's the case.
+        if System.String.IsNullOrWhiteSpace s then reraise ()
+        else
+            // Suppress the original exception and let 'checkPathForIllegalChars' raise a new one.
+            checkPathForIllegalChars s
+            // In case the exception was raised for some other reason.
+            reraise ()
 
 let directoryName (s:string) = 
-    checkPathForIllegalChars s
-    if s = "" then "."
-    else 
-      match Path.GetDirectoryName(s) with 
-      | null -> if FileSystem.IsPathRootedShim(s) then s else "."
-      | res -> if res = "" then "." else res
+    if s.Length = 0 then "."
+    else
+        let dirName =
+            try Path.GetDirectoryName(s)
+            with
+            | :? System.ArgumentException ->
+                // Path.GetDirectoryName(string) can raise ArgumentException when the string is empty or whitespace.
+                // Check for that condition and propagate the original exception if it's the case.
+                if System.String.IsNullOrWhiteSpace s then reraise ()
+                else
+                    // Suppress the original exception and let 'checkPathForIllegalChars' raise a new one.
+                    checkPathForIllegalChars s
+                    // In case the exception was raised for some other reason.
+                    reraise ()
+
+        match dirName with
+        | null -> if FileSystem.IsPathRootedShim(s) then s else "."
+        | res -> if res.Length = 0 then "." else res
 
 let fileNameOfPath s = 
-    checkPathForIllegalChars s
-    Path.GetFileName(s)        
+    // This is implemented in such a way that the original behavior is preserved
+    // (where 'checkPathForIllegalChars' used to be called first to validate the input).
+    // This implementation avoids that call unless necessary, so is more efficient.
+    try Path.GetFileName(s)
+    with
+    | :? System.ArgumentException ->
+        // Suppress the original exception and let 'checkPathForIllegalChars' raise a new one.
+        checkPathForIllegalChars s
+        // In case the exception was raised for some other reason.
+        reraise ()
 
 let fileNameWithoutExtension s = 
-    checkPathForIllegalChars s
-    Path.GetFileNameWithoutExtension(s)        
+    try Path.GetFileNameWithoutExtension(s)
+    with
+    | :? System.ArgumentException ->
+        // Suppress the original exception and let 'checkPathForIllegalChars' raise a new one.
+        checkPathForIllegalChars s
+        // In case the exception was raised for some other reason.
+        reraise ()
